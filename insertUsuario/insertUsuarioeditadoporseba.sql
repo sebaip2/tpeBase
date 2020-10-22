26a69b8cf67fb94b28972944d404e4bf507c8025
sebaip2 <44104852+sebaip2@users.noreply.github.com>
2020-10-22 00:04:13 -0300

    Update and rename insertUsuario.sql to insertUsuarioeditadoporseba.sql

---
+++ insertUsuario/insertUsuarioeditadoporseba.sql
@@ -0,0 +1,39 @@
+------------------------------------------------USUARIOS---------------------------------------------------------------------------
+
+INSERT INTO G16_USUARIO(id_usuario, apellido, nombre, fecha_alta, estado, email, password, telefono, id_pais)
+VALUES (570948, 'Ulibarri', 'Sebastian', now() - '2 year'::interval * random(),'falta', 'sebastianulibarri@gmail.com', '570948', 570948, 1);
+
+INSERT INTO G16_USUARIO(id_usuario, apellido, nombre, fecha_alta, estado, email, password, telefono, id_pais)
+VALUES (569692, 'Regert', 'Felix', now() - '2 year'::interval * random(),'falta', 'felixregert@gmail.com', '569692', 569692, 1);
+
+INSERT INTO G16_USUARIO(id_usuario, apellido, nombre, fecha_alta, estado, email, password, telefono, id_pais)
+VALUES (226215, 'Abachian', 'Joaquin', now() - '2 year'::interval * random(),'falta', 'joaquinabachian@gmail.com', '226215', 226215, 1);
+
+INSERT INTO G16_USUARIO(id_usuario, apellido, nombre, fecha_alta, estado, email, password, telefono, id_pais)
+VALUES (543501, 'Ascazuri','Federico', now() - '2 year'::interval * random(),'falta', 'federicoazcazuri@gmail.com', '543501', 543501, 1);
+
+INSERT INTO G16_USUARIO(id_usuario, apellido, nombre, fecha_alta, estado, email, password, telefono, id_pais)
+VALUES (515722, 'Raineri', 'Franco', now() - '2 year'::interval * random(),'falta', 'francoraineri@gmail.com', '515722', 515722, 1);
+
+INSERT INTO G16_USUARIO(id_usuario, apellido, nombre, fecha_alta, estado, email, password, telefono, id_pais)
+VALUES (338652, 'Verde', 'Jeremias', now() - '2 year'::interval * random(),'falta', 'jeremiasverde@gmail.com', '338652', 338652, 2);
+
+INSERT INTO G16_USUARIO(id_usuario, apellido, nombre, fecha_alta, estado, email, password, telefono, id_pais)
+VALUES (977214, 'Dure', 'Juan', now() - '2 year'::interval * random(),'falta', 'juandure@gmail.com', '977214', 977214, 2);
+
+INSERT INTO G16_USUARIO(id_usuario, apellido, nombre, fecha_alta, estado, email, password, telefono, id_pais)
+VALUES (898476, 'Ledesma', 'Lisandro', now() - '2 year'::interval * random(),'falta', 'lisandroledesma@gmail.com', '898476', 898476, 2);
+
+INSERT INTO G16_USUARIO(id_usuario, apellido, nombre, fecha_alta, estado, email, password, telefono, id_pais)
+VALUES (485485, 'Pedro', 'Procopio', now() - '2 year'::interval * random(),'falta', 'pedroprocopio@gmail.com', '485485', 485485, 2);
+
+INSERT INTO G16_USUARIO(id_usuario, apellido, nombre, fecha_alta, estado, email, password, telefono, id_pais)
+VALUES (683720, 'Tobias','Malbos', now() - '2 year'::interval * random(),'falta', 'tobiasmalbos@gmail.com', '683720', 683720, 2);
+
+INSERT INTO G16_USUARIO(id_usuario, apellido, nombre, fecha_alta, estado, email, password, telefono, id_pais)
+VALUES (378429, 'Jeremias', 'Salinas', now() - '2 year'::interval * random(),'falta', 'jeremiassalinas@gmail.com', '378429', 378429, 2);
+
+INSERT INTO G16_USUARIO(id_usuario, apellido, nombre, fecha_alta, estado, email, password, telefono, id_pais)
+VALUES (521624, 'Arellano', 'Franco' , now() - '2 year'::interval * random(),'falta', 'francoarellano@gmail.com', '521624', 521624, 2);
+
+
